b5175fd8c646d2804b5e335923dbe3c2070f25da
Ian <ianlum314@gmail.com>
2023-07-08 12:35:35 -0700

    instantiate num_interactions

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,16 +4,25 @@ var npc_interaction_available = false
 
 const DIALOGUE_PATH: String = "res://assets/dialogue/dialogue.json"
 var dialog
+var num_interactions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HBoxContainer/VBoxContainer/NinePatchRect/DialoguePrompt.hide()
 	$HBoxContainer/VBoxContainer/NinePatchRect/DialogueBox.hide()
 	
+	# Load dialogue json
 	assert(FileAccess.file_exists(DIALOGUE_PATH), "Dialog file at %s does not exist" % DIALOGUE_PATH)
 	
 	var json_str = FileAccess.open(DIALOGUE_PATH, FileAccess.READ).get_as_text()
-	var dialog = JSON.parse_string(json_str)
+	dialog = JSON.parse_string(json_str)
+	
+	# Instantiate num_interactions
+	num_interactions = dialog.duplicate()
+	for name in num_interactions.keys():
+		num_interactions[name] = 0
+		
+	print(num_interactions)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
