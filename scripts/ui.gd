09aa9f4a727fc5281091290e7dfd374050c799de
Ian <ianlum314@gmail.com>
2023-07-08 13:05:56 -0700

    select dialogue number based on number of interactions

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,7 +4,7 @@ var current_interactable_npc = null
 var in_interaction = false
 
 const DIALOGUE_PATH: String = "res://assets/dialogue/dialogue.json"
-var dialog
+var dialogue
 var num_interactions
 
 # Called when the node enters the scene tree for the first time.
@@ -16,20 +16,20 @@ func _ready():
 	assert(FileAccess.file_exists(DIALOGUE_PATH), "Dialog file at %s does not exist" % DIALOGUE_PATH)
 	
 	var json_str = FileAccess.open(DIALOGUE_PATH, FileAccess.READ).get_as_text()
-	dialog = JSON.parse_string(json_str)
+	dialogue = JSON.parse_string(json_str)
 	
 	# Instantiate num_interactions
-	num_interactions = dialog.duplicate()
+	num_interactions = dialogue.duplicate()
 	for name in num_interactions.keys():
 		num_interactions[name] = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(num_interactions)
 	if Input.is_action_just_pressed("interact") and current_interactable_npc:
 		$HBoxContainer/VBoxContainer/NinePatchRect/DialogueBox.show()
 		$HBoxContainer/VBoxContainer/NinePatchRect/DialoguePrompt.hide()
 		$HBoxContainer/VBoxContainer/NinePatchRect/DialogueBox/Label.text = current_interactable_npc
+		load_npc_dialogue(current_interactable_npc)
 		if not in_interaction:
 			num_interactions[current_interactable_npc] += 1
 			in_interaction = true
@@ -47,4 +47,11 @@ func _on_npc_dialogue_collider_area_exited(area):
 		current_interactable_npc = null
 		in_interaction = false
 
-#func load_npc_dialogue(name):
+func load_npc_dialogue(name):
+	var dialogue_options = dialogue[name]
+	var interaction_limits = dialogue_options.keys()
+	interaction_limits.reverse()
+	for limit in interaction_limits:
+		if num_interactions[name] >= int(limit):
+			print(limit)
+			break
