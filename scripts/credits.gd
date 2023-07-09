79f5d2da9501986ac46c404aac7010ca62efb172
Josh <joshuatehrlich@gmail.com>
2023-07-09 12:29:12 -0400

    final

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -17,5 +17,6 @@ func _on_timer_timeout():
 
 func _on_fade_animation_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
+		print('asdfasfadsfasdf')
 		$FadeBlack.hide()
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,10 @@
 extends CanvasLayer
 
-
+@onready var fade_animation: AnimationPlayer = $FadeBlack/FadeAnimation
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$FadeBlack.show()
+	fade_animation.play("fade_from_black")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,8 +12,16 @@ func _process(delta):
 	pass
 
 func _on_start_button_pressed():
-	print('start')
+	$FadeBlack.show()
+	fade_animation.play("fade_to_black")
 	get_tree().change_scene_to_file("res://levels/level_0.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_fade_animation_animation_finished(anim_name):
+	if anim_name == "fade_from_black":
+		$FadeBlack.hide()
+	if anim_name == "fade_to_black":
+		$FadeBlack.hide()
+		get_tree().change_scene_to_file("res://levels/level_0.tscn")
