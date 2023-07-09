13ca45692533ee4c9d9f533ed2da40a7ebfc85fa
Ian <ianlum314@gmail.com>
2023-07-09 02:25:33 -0700

    add camera smooth enable collider

--- scripts/PlayerCamera.gd
+++ scripts/PlayerCamera.gd
@@ -4,12 +4,7 @@ extends Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.limit_smoothed = false
-	$EnableSmoothTimer.start(1)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _on_enable_smooth_timer_timeout():
-	self.limit_smoothed = true
-	print("1")
+func _on_smooth_enable_point_body_entered(body):
+	if body.name == 'player':
+		self.limit_smoothed = true
